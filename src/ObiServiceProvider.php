b5f774fab285c4fdbed33f14342bbb221e6d1756
Karim Mohamed <komicho1996@gmail.com>
2025-11-04 00:45:24 +0200

    Fix autoload namespace and add ObiServiceProvider class

---
+++ src/ObiServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Obelaw\Obi;
+
+use Illuminate\Support\ServiceProvider;
+
+class ObiServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
